788a75a535432a19a93ed342571c449558d2a83b
wangso <wangso@missouri.edu>
2016-04-30 16:10:18 -0500

    add priviledges

--- db/DDL/users_create.sql
+++ db/DDL/users_create.sql
@@ -37,16 +37,27 @@ GRANT INSERT, UPDATE, DELETE, SELECT ON photoarchiving.* TO weijian WITH GRANT O
 CREATE USER 'web_user'@'localhost' IDENTIFIED BY 'sSK80rkyYAdzx3LjWpSN';
 
 #	Grant the web user privileges on all tables
-/*
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving. ;
-*/
+
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.era ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.life_period ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.person ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.non_user ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_auth ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_pref ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.repository ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.family_repository ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.location_repository ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_repo ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.person_relation;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.location ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photograph ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_tag ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.story ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_story ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_activity ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.log ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.session_log ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.activity_log ;
+GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_repo ;
+
