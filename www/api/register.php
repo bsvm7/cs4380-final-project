aa67016aca502e6f9d01a807df4c6add9b37a561
Tony Forsythe <forsythetony@gmail.com>
2016-05-01 16:43:59 -0500

    Wow

--- www/api/register.php
+++ www/api/register.php
@@ -9,7 +9,18 @@
 	$db_pass = constant("DB_PASS");
 	$db_database = constant("DB_DATABASE");
 	
+	//	First connect to the database using values from the included file
+	$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_database);
 	
-	echo "\nThe user for the database is $db_user\nThe host is:\t$db_host\nThe password is:\t$db_pass\nThe database is:\t$db_database\n";
+	if ($db_conn->error_code) {
+		
+		set_error_response( 400 , "I couldn't connect to the database -> " . $db_conn->connect_error);
+		die("The connection to the database failed: " . $db_conn->connect_error);
+	}
+	else {
+		http_response_code(200);
+		echo "I did it!";
+	}
+
 	
 ?>
