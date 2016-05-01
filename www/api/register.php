e603b508a59d69d3e4fa2cda8039791b19dd40ed
Tony Forsythe <forsythetony@gmail.com>
2016-05-01 16:41:35 -0500

    Wow

--- www/api/register.php
+++ www/api/register.php
@@ -5,7 +5,11 @@
 	include("../../../db_security/security.php");
 	
 	$db_user = constant("DB_USER");
+	$db_host = constant("DB_HOST");
+	$db_pass = constant("DB_PASS");
+	$db_database = constant("DB_DATABASE");
 	
-	echo "\nThe user for the database is $db_user\n";
+	
+	echo "\nThe user for the database is $db_user\nThe host is:\t$db_host\nThe password is:\t$db_pass\nThe database is:\t$db_database\n";
 	
 ?>
