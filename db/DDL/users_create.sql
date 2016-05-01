52fde93e96ece66b6cf3c9dce8ef34a46ba840bb
wangso <wangso@missouri.edu>
2016-04-30 21:39:17 -0500

    fixed wrong syntax

--- db/DDL/users_create.sql
+++ db/DDL/users_create.sql
@@ -43,26 +43,26 @@ CREATE USER 'web_user'@'localhost' IDENTIFIED BY 'sSK80rkyYAdzx3LjWpSN';
 
 #	Grant the web user privileges on all tables
 
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.era ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.life_period ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.person ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.non_user ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_auth ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_pref ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.repository ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.family_repository ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.location_repository ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_repo ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.person_relation;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.location ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photograph ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_tag ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.story ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_story ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.user_activity ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.log ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.session_log ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.activity_log ;
-GRANT UPDATE, INSERT, SELECT, DELETE TO 'web_user'@'localhost' ON photoarchiving.photo_repo ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.era TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.life_period TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.person TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.non_user TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.user TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.user_auth TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.user_pref TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.repository TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.family_repository TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.location_repository TO 'web_user'@'localhost'  ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.user_repo TO 'web_user'@'localhost'  ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.person_relation TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.location TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.photograph TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.photo_tag TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.story TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.photo_story TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.user_activity TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.log TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.session_log TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.activity_log TO 'web_user'@'localhost' ;
+GRANT UPDATE, INSERT, SELECT, DELETE ON photoarchiving.photo_repo TO 'web_user'@'localhost' ;
 
