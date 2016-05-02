af4ee546025d906a6c627d92468c9f8f73eb3cef
Tony Forsythe <forsythetony@gmail.com>
2016-05-01 22:01:06 -0500

    Fix

--- db/DDL/input_data.sql
+++ db/DDL/input_data.sql
@@ -70,7 +70,6 @@ SET
 set foreign_key_checks = 0;
 LOAD DATA LOCAL INFILE '../input_data/photo_story.csv' INTO TABLE photo_story
 FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 ( p_id, s_id );
