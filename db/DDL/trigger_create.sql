59670804948bd06c26e6bbb3650c74947325752b
Tony Forsythe <forsythetony@gmail.com>
2016-05-02 23:22:02 -0500

    Adds a new trigger and table for archiving old photographs

--- db/DDL/trigger_create.sql
+++ db/DDL/trigger_create.sql
@@ -45,3 +45,19 @@ BEGIN
 	END IF;
 END;//
 delimiter ;
+
+
+#	A trigger to archive all deleted photographs in the archived_photographs table
+DROP TRIGGER IF EXISTS archive_photographs;
+
+DELIMITER //
+CREATE TRIGGER archive_photographs BEFORE DELETE ON photograph
+FOR EACH ROW
+BEGIN
+	SET arch_photo_url_large = OLD.large_url;
+	SET arch_photo_url_thumb = OLD.thumb_url;
+	SET arch_photo_title = OLD.title;
+	SET arch_repo_title = (SELECT R.name FROM repository R, photo_repo PR WHERE PR.p_id = OLD.p_id AND PR.r_id = R.r_id LIMIT 1);
+	SET arch_photo_date_uploaded = OLD.date_uploaded;
+END;//
+delimiter ;
