0eb2773f9c13a7186a7e1d8cc1229e3e31dd6d9b
Tony Forsythe <forsythetony@gmail.com>
2016-05-06 18:31:43 -0500

    Adds both the login page and the login controllers

---
+++ www/js/controllers/loginController.js
@@ -0,0 +1,82 @@
+var app = angular.module('photoarchiving_app', [])
+  .controller('LoginController', function($scope, $http, $window) {
+  	
+	//	CONSTANTS
+	var base_url_main = "http://www.cs4380-group9.tech/final_project/";
+	var base_url = base_url_main;
+
+
+	
+	$scope.credentials = {
+		'username' : '',
+		'password' : ''
+		
+	};
+	
+	checkIfLoggedIn();
+
+	function checkIfLoggedIn() {
+		
+		if (typeof(Storage) !== "undefined") {
+			
+			if (sessionStorage.auth_token) {
+				$window.location.href = base_url + "home.php";
+			}
+		}
+		
+	}
+	
+	
+
+	$scope.authenticate = function() {
+		
+		var auth_url = base_url + "api/authorization.php";
+		
+		var auth_url = auth_url + "?authtype=initial&" + "username=" + $scope.credentials.username + "&password=" + $scope.credentials.password;
+		
+		console.log("The auth url was -> " + auth_url);
+		
+		
+		$http({
+			method : 'GET',
+			url : auth_url
+		}).then(function successCallBack(response) {
+			
+			//	Pull auth_token data from the response
+			var auth_token = response.data.auth_token;
+			var expires_in = response.data.expires_in;
+			
+			console.log( "The auth_token (1) is -> " + auth_token);
+			//	Set the auth token data in storage
+			if (typeof(Storage) !== "undefined") {
+				sessionStorage.auth_token = auth_token;
+				sessionStorage.expires_in = expires_in;
+			}
+			
+			//	Redirect the user to the home page
+			var redirect_url = base_url + "company.php?cid=2";
+			
+			$window.location.href = redirect_url;
+			
+		}, function errorCallback(response) {
+			
+			console.log("There was an error");
+			
+		});
+		
+		
+		
+	}
+	
+	function get_base_url() {
+		
+		if (use_main_url) {
+			return settings.base_url;
+		}
+		else {
+			return settings.dev_base_url;
+		}
+	}
+
+	
+  });
---
+++ www/login.php
@@ -0,0 +1,131 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Login | LinkedIn</title>
+
+	<!-- Angular -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js></script>
+	
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- CUSTOM STYLES
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    
+    <link rel="stylesheet" href="css/custom.css">
+    
+    <style type="text/css">
+    
+    body, html {
+      background-color: #e7e9ec;
+      font-family: Helvetica, Arial, sans-serif;
+    }
+    .box {
+      margin-top: 10px;
+      padding-top: 40px;
+      padding-bottom: 40px;
+      background-color: white;
+      -webkit-box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+      -moz-box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+      box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+    }
+    .location {
+      margin-top: 0px;
+      margin-bottom: 10px;
+    }
+    .company-name {
+      margin-bottom: 0px;
+      font-size: 60px;
+    }
+    .company-desc {
+      text-align: left;
+    }
+    .footer p {
+      margin: 15px 0px;
+    }
+    .form-group label {
+      color: #66696A;
+      font-weight: 500;
+      margin-bottom: 0px;
+    }
+    .create-button {
+      margin-top: 30px;
+    }
+    
+    </style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body ng-app="photoarchiving_app">
+	  
+  	<script type="text/javascript" src="js/controllers/loginController.js"></script>
+  	
+    <!-- NAVBAR
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/index.php">LinkedIn</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/register.php">Register</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- REGISTER
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="container" ng-controller="LoginController as loginctrl">
+      <div class="row">
+        <div class="col-md-6 text-center col-md-offset-3 box">
+          <h1 class="company-name">LinkedIn</h1>
+          <h2 class="lead location">Login To Your Account</h2>
+          
+          <div class="col-md-10 col-md-offset-1 text-left">
+            <form>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" name="username" ng-model="credentials.username">
+              </div>
+              <div class="form-group">
+                <label for="password">Password (6 or more characters)</label>
+                <input type="password" class="form-control" id="password" name="password" ng-model="credentials.password">
+              </div>
+              
+              <button type="submit" class="btn btn-primary btn-block btn-lg create-button" ng-click="authenticate()">Login</button>
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>    
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
