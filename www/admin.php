3a047210704cea6f0a701d86af6328a5c8f61721
Tony Forsythe <forsythetony@gmail.com>
2016-05-07 15:04:42 -0500

    Adds two pages for admin and repo admin
    Adds highcharts for bower

---
+++ www/admin.php
@@ -0,0 +1,105 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home | Photoarchiving</title>
+
+	<!-- Angular -->
+	<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
+	
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- CUSTOM STYLES
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    
+    <link rel="stylesheet" href="css/custom.css">
+    
+    <style type="text/css">
+    
+    body, html {
+      background-color: #e7e9ec;
+      font-family: Helvetica, Arial, sans-serif;
+    }
+    .box {
+      margin-top: 10px;
+      padding-top: 40px;
+      padding-bottom: 40px;
+      background-color: white;
+      -webkit-box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+      -moz-box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+      box-shadow: 0 1px 1px rgba(0,0,0,0.15),-1px 0 0 rgba(0,0,0,0.03),1px 0 0 rgba(0,0,0,0.03),0 1px 0 rgba(0,0,0,0.12);
+    }
+    .location {
+      margin-top: 0px;
+      margin-bottom: 10px;
+    }
+    .company-name {
+      margin-bottom: 0px;
+      font-size: 60px;
+    }
+    .company-desc {
+      text-align: left;
+    }
+    .footer p {
+      margin: 15px 0px;
+    }
+    .form-group label {
+      color: #66696A;
+      font-weight: 500;
+      margin-bottom: 0px;
+    }
+    .create-button {
+      margin-top: 30px;
+    }
+    
+    </style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body ng-app="photoarchiving_app" ng-controller="HomeController as homeCtrl">
+	  
+  	<script type="text/javascript" src="js/controllers/homeController.js"></script>
+  	
+    <!-- NAVBAR
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="cs4380/home.php">Photoarchiving</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+	          <li ng-show="{{ user_info.user_level == 1 }}"><a href="cs4380/admin.php">Admin</a></li>
+            <li><a href="cs4380/logout.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>  
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
