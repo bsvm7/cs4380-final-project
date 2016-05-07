954841f5ac548e8a6c9518b4d692abeba25a4035
Tony Forsythe <forsythetony@gmail.com>
2016-05-06 23:07:07 -0500

    Adds photo and repository API endpoints

---
+++ www/api/repository.php
@@ -0,0 +1,140 @@
+<?php
+	
+	
+	//	Include reference to sensitive databse information
+	include("../../../db_security/security.php");
+	
+	$db_user = constant("DB_USER");
+	$db_host = constant("DB_HOST");
+	$db_pass = constant("DB_PASS");
+	$db_database = constant("DB_DATABASE");
+	
+	//	First connect to the database using values from the included file
+	$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_database);
+	
+	if ($db_conn->error_code) {
+		
+		set_error_response( 400 , "I couldn't connect to the database -> " . $db_conn->connect_error);
+		die("The connection to the database failed: " . $db_conn->connect_error);
+	}
+		
+	debug_echo( "database connected" . "\n" );
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+		UTILITY FUNCTIONS
+	*/
+	function clean_date( $date_string ) {
+		
+		$is_valid_date = true;
+		
+		$clean_date_string;
+		
+		$pieces = explode("-", $date_string);
+		
+		
+		if (count($pieces) == 3) {
+			
+			$year = intval($pieces[2]);
+			$valid_year = true;
+			$month = intval($pieces[0]);
+			$valid_month = true;
+			$day = intval($pieces[1]);
+			$valid_day = true;
+			
+			$min_age = 15;
+			
+			
+			$max_year = intval(date("Y")) - $min_age;
+			
+			if (!($year >= 1900 && $year < $max_year)) {
+				$valid_year = false;
+			}
+			
+			if (!($month >= 1 && $month <= 12)) {
+				$valid_month = false;
+			}
+			
+			if ($valid_year && $valid_month) {
+				
+				$max_day = cal_days_in_month(CAL_GREGORIAN, $valid_month, $valid_year);
+				
+				
+				if ($day > 0 && $day <= $max_day) {
+					$is_valid_date = true;
+				}
+			}
+			else {
+				$is_valid_date = false;
+			}
+			
+			if ($is_valid_date) {
+				
+				$month_string;
+				$day_string;
+				
+				
+				if ($day < 10) {
+					$day_string = "0" . $day;
+				}
+				else {
+					$day_string = "$day";
+				}
+				
+				if ($month < 10) {
+					$month_string = "0" . $month;
+				}
+				else {
+					$month_string = "$month";
+				}
+				
+				$clean_date_string = $year . "/" . $month_string . "/" . $day_string;
+			}
+		}
+		
+		$ret_array = array(
+			"isValidDate" => $is_valid_date,
+			"validDateString" => $clean_date_string
+		);
+
+		return $ret_array;
+	}
+
+
+	function debug_echo( $str ) {
+		
+		$echo_debug = true;
+		
+		if ($echo_debug) {
+			echo $str;
+		}
+	}
+	
+	function set_error_response( $error_code , $error_message ) {
+		
+		
+		$http_response_code = 500;
+		
+		$response_array = array(
+			"error_code" => $error_code,
+			"error_message" => $error_message
+		);
+				echo json_encode($response_array);
+		http_response_code($error_code);
+		
+	}
+	
+	
+?>
