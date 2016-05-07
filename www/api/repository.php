dc7cc368ab38173fb13dc43ce039d12027389c99
wangso <wangso@missouri.edu>
2016-05-07 14:33:27 -0500

    up

--- www/api/repository.php
+++ www/api/repository.php
@@ -21,6 +21,7 @@
 
 	debug_echo ("server connected successfully"."\n");
 
+/*
 
 	$req_method = $_SERVER['REQUEST_METHOD'];	
 
@@ -193,105 +194,10 @@
 
 	}	
 
-
-function set_error_response( $error_code , $error_message ) {
-	
-	
-	$http_response_code = 500;
-	
-	$response_array = array(
-		"error_code" => $error_code,
-		"error_message" => $error_message
-	);
-	
-	echo json_encode($response_array);
-	
-	http_response_code($error_code);
-	
-}
+*/	
 
 
 	
-	/*
-		UTILITY FUNCTIONS
-	*/
-	function clean_date( $date_string ) {
-		
-		$is_valid_date = true;
-		
-		$clean_date_string;
-		
-		$pieces = explode("-", $date_string);
-		
-		
-		if (count($pieces) == 3) {
-			
-			$year = intval($pieces[2]);
-			$valid_year = true;
-			$month = intval($pieces[0]);
-			$valid_month = true;
-			$day = intval($pieces[1]);
-			$valid_day = true;
-			
-			$min_age = 15;
-			
-			
-			$max_year = intval(date("Y")) - $min_age;
-			
-			if (!($year >= 1900 && $year < $max_year)) {
-				$valid_year = false;
-			}
-			
-			if (!($month >= 1 && $month <= 12)) {
-				$valid_month = false;
-			}
-			
-			if ($valid_year && $valid_month) {
-				
-				$max_day = cal_days_in_month(CAL_GREGORIAN, $valid_month, $valid_year);
-				
-				
-				if ($day > 0 && $day <= $max_day) {
-					$is_valid_date = true;
-				}
-			}
-			else {
-				$is_valid_date = false;
-			}
-			
-			if ($is_valid_date) {
-				
-				$month_string;
-				$day_string;
-				
-				
-				if ($day < 10) {
-					$day_string = "0" . $day;
-				}
-				else {
-					$day_string = "$day";
-				}
-				
-				if ($month < 10) {
-					$month_string = "0" . $month;
-				}
-				else {
-					$month_string = "$month";
-				}
-				
-				$clean_date_string = $year . "/" . $month_string . "/" . $day_string;
-			}
-		}
-		
-		$ret_array = array(
-			"isValidDate" => $is_valid_date,
-			"validDateString" => $clean_date_string
-		);
-
-		return $ret_array;
-	}
-
-
 	function debug_echo( $str ) {
 		
 		$echo_debug = true;
@@ -315,6 +221,6 @@ function set_error_response( $error_code , $error_message ) {
 		
 	}
 	
-	
+
 ?>
 
