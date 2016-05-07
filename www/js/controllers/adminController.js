249ae3175046d4d854ac9022286e9bb277c16c50
Tony Forsythe <forsythetony@gmail.com>
2016-05-07 15:28:30 -0500

    Fix

--- www/js/controllers/adminController.js
+++ www/js/controllers/adminController.js
@@ -17,69 +17,80 @@ var app = angular.module('photoarchiving_app', ["highcharts-ng"])
 	//checkIfLoggedIn();
 	//gather_user_information();
 	
-	//console.log( $scope.user_info );
 	
 	$scope.chartConfig = {
 
-  options: {
-      //This is the Main Highcharts chart config. Any Highchart options are valid here.
-      //will be overriden by values specified below.
-      chart: {
-          type: 'bar'
-      },
-      tooltip: {
-          style: {
-              padding: 10,
-              fontWeight: 'bold'
-          }
-      }
-  },
-  //The below properties are watched separately for changes.
-
-  //Series object (optional) - a list of series using normal Highcharts series options.
-  series: [{
-     data: [10, 15, 12, 8, 7]
-  }],
-  //Title configuration (optional)
-  title: {
-     text: 'Hello'
-  },
-  //Boolean to control showing loading status on chart (optional)
-  //Could be a string if you want to show specific loading text.
-  loading: false,
-  //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-  //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
-  xAxis: {
-  currentMin: 0,
-  currentMax: 20,
-  title: {text: 'values'}
-  },
-  //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
-  useHighStocks: false,
-  //size (optional) if left out the chart will default to size of the div or something sensible.
-  size: {
-   width: 400,
-   height: 300
-  },
-  //function (optional)
-  func: function (chart) {
-   //setup some logic for the chart
-  }
-};
-
-
-
-
+	    options: {
+	        //This is the Main Highcharts chart config. Any Highchart options are valid here.
+	        //will be overriden by values specified below.
+	        chart: {
+	            type: 'bar'
+	        },
+	        tooltip: {
+	            style: {
+	                padding: 10,
+	                fontWeight: 'bold'
+	            }
+	        }
+	    },
+	    //The below properties are watched separately for changes.
+	
+	    //Series object (optional) - a list of series using normal Highcharts series options.
+	    series: [{
+	        data: []
+	    }],
+	    //Title configuration (optional)
+	    title: {
+	        text: 'Hello'
+	    },
+	    //Boolean to control showing loading status on chart (optional)
+	    //Could be a string if you want to show specific loading text.
+	    loading: false,
+	    //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+	    //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
+	    xAxis: {
+	        currentMin: 0,
+	        currentMax: 1,
+	        title: {
+	            text: 'values'
+	        }
+	    },
+	    //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
+	    useHighStocks: false,
+	    //size (optional) if left out the chart will default to size of the div or something sensible.
+	    size: {
+	        width: 400,
+	        height: 300
+	    },
+	    //function (optional)
+	    func: function(chart) {
+	        //setup some logic for the chart
+	    }
+	};
 
 
 
 
 
 
+	load_dummy_data();
 
 
 
 
+
+
+	function load_dummy_data() {
+		
+		var dummy_series = [ 90 , 80 , 20 , 11 , 2 , 33 , 5 ];
+		var dummy_max = 100;
+		var dummy_min = 0;
+		
+		$scope.chartConfig.series[0].data = dummy_series;
+		$scope.chartConfig.xAxis.currentMin = 0;
+		$scope.chartConfig.xAxis.currentMax = 100;
+		
+	}
 
 
 
