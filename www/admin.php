1f6d5766aa984ac988f1f4a1491f440af4134a90
wldzc <weijian.li.china@gmail.com>
2016-05-08 18:17:59 -0500

    Fix

--- www/admin.php
+++ www/admin.php
@@ -65,9 +65,13 @@
       margin-top: 30px;
     }
     
-.highcharts-container{
-    width:100%;   
-}
+	.highcharts-container{
+		width:100%;   
+	}
+
+	.highcharts-background{
+		margin: auto;
+	}
     </style>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
