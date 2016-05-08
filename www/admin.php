306792244919451534aee7f4a1c991cac4d345d2
wldzc <weijian.li.china@gmail.com>
2016-05-08 15:31:40 -0500

    Fix

--- www/admin.php
+++ www/admin.php
@@ -17,7 +17,7 @@
 	<!-- Highcharts -->
 	<script src="https://code.highcharts.com/highcharts.js"></script>
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
-	<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+	
 
     <!-- CUSTOM STYLES
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -99,7 +99,7 @@
       </div>
     </nav>  
 
-
+<div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
 <script>
 $(function () {
 
