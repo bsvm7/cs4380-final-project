2b96f3fd81982beee5a3a11abeac7a8bd68527b5
wldzc <weijian.li.china@gmail.com>
2016-05-08 14:13:15 -0500

    analytic test

--- www/js/controllers/adminController.js
+++ www/js/controllers/adminController.js
@@ -18,64 +18,253 @@ var app = angular.module('photoarchiving_app', ["highcharts-ng"])
 	//gather_user_information();
 	
 	
-	$scope.chartConfig = {
+	var colorArr = ["#ffcd00", "#009dd9",  "#ff8300", "#b21dac",  "#d70036", "#707276", "#aaaaaa", "#000000", "#218535", "#92d050","#c4efff"];
+// nannv chart
+$(function () {
+    $('#content1').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45
+            }
+        },
+        colors: colorArr,
+        title: {
+            text: 'Age distribution'
+        },
+        subtitle: {
+            text: ''
+        },
+        plotOptions: {
+            pie: {
+                 depth: 45
+            }
+        },
+        series: [{
+            name: 'number',
+            data: [
+                ['10~20', <?php echo $data; ?>],
+                ['21~30', 10],
+                ['31~40', 10],
+                ['41~50', 10],
+                ['51~60', 10],
+                ['60~above', 10]
+            ]
+        }]
+    });
+});
 
-	    options: {
-	        //This is the Main Highcharts chart config. Any Highchart options are valid here.
-	        //will be overriden by values specified below.
-	        chart: {
-	            type: 'bar'
-	        },
-	        tooltip: {
-	            style: {
-	                padding: 10,
-	                fontWeight: 'bold'
-	            }
-	        }
-	    },
-	    //The below properties are watched separately for changes.
-	
-	    //Series object (optional) - a list of series using normal Highcharts series options.
-	    series: [{
-	        data: []
-	    }],
-	    //Title configuration (optional)
-	    title: {
-	        text: 'Hello'
-	    },
-	    //Boolean to control showing loading status on chart (optional)
-	    //Could be a string if you want to show specific loading text.
-	    loading: false,
-	    //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-	    //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
-	    xAxis: {
-	        currentMin: 0,
-	        currentMax: 1,
-	        title: {
-	            text: 'values'
-	        }
-	    },
-	    yAxis: {
-		    currentMin: 0,
-		    currentMax: 1
-	    },
-	    //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
-	    useHighStocks: false,
-	    //size (optional) if left out the chart will default to size of the div or something sensible.
-	    size: {
-	        width: 400,
-	        height: 300
-	    },
-	    //function (optional)
-	    func: function(chart) {
-	        //setup some logic for the chart
-	    }
-	};
+        var width = $("#tab2").width();
+        
+//        line chart
+ $(function () {
+    $('#content2').highcharts({
+        title: {
+            text: 'System Traffic Trend Monitor',
+            x: -20 //center
+        },
+        subtitle: {
+            text: '(09/03/2016 - 21/03/2016)',
+            x: -20
+        },
+        xAxis: {
+            categories: ['day1', 'day2', 'day3', 'day4', 'day5', 'day6',
+                'day7', 'day8', 'day9', 'day10', 'day11', 'day12']
+        },
+        yAxis: {
+            title: {
+                text: '# of login'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '#'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            color: colorArr[1],
+            name: 'Traffic',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        },]
+    });
+});
+        
+        
+//        accumulated column
+        $(function () {
+    $('#content3').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Instructor Activism Monitor'
+        },
+        xAxis: {
+            categories: ['ins1', 'ins2', 'ins3', 'ins4', 'ins5']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            valueSuffix: '%',
+            headerFormat: '<b>{point.x}</b><br/>',
+           
+            pointFormat: '{series.name}: {point.percentage:.1f}%<br/>总量: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: '# video submitted',
+            color: colorArr[0],
+            data: [300, 300, 40, 70, 200]
+        }, {
+            name: '# like&fav',
+            color:  colorArr[1],
+            data: [200, 400, 300, 200, 10]
+        }, {
+            name: '# comments',
+            color: colorArr[2],
+            data: [30, 200, 400, 200, 300]
+        }]
+    });
+});
+        
+//fourth chart        
+$(function () {
+    $('#content4').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45
+            }
+        },
+        colors: colorArr,
+        title: {
+            text: 'instructor video post distribution(7 day ago)'
+        },
+        subtitle: {
+            text: ''
+        },
+        plotOptions: {
+            pie: {
+                 depth: 45
+            }
+        },
+        series: [{
+            name: 'number',
+            data: [
+                ['ins1', <?php echo $data; ?>],
+                ['ins2', 10],
+                ['ins3', 10],
+                ['ins4', 10]
+            ]
+        }]
+    });
+});
 
-
-
-
-
+//fifth chart
+   $(function () {
+    $('#content5').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Popular video Rank'
+        },
+        xAxis: {
+            categories: ['ins1', 'ins2', 'ins3', 'ins4', 'ins5']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            valueSuffix: '%',
+            headerFormat: '<b>{point.x}</b><br/>',
+           
+            pointFormat: '{series.name}: {point.percentage:.1f}%<br/>总量: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: '# video submitted',
+            color: colorArr[1],
+            data: [300, 200, 140, 70, 30]
+        }]
+    });
+});
 
 	load_dummy_data();
 
