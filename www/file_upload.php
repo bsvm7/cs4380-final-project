cba8dcdc62323fccbe3fbfc47071dc1b3a60fdb8
wangso <wangso@missouri.edu>
2016-05-08 22:12:55 -0500

    up

--- www/file_upload.php
+++ www/file_upload.php
@@ -4,7 +4,7 @@
 <title>File Upload and view With PHP and MySql</title>
 </head>
 <body>
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="./api/upload.php" method="post" enctype="multipart/form-data">
 <input type="file" name="your_photo" />
 <button type="submit" name="btn-upload">upload</button>
 </form>
