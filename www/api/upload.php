1640a43720b6f7003513ea9ab1fea27523e63b0c
wangso <wangso@missouri.edu>
2016-05-08 22:01:41 -0500

    update

--- www/api/upload.php
+++ www/api/upload.php
@@ -95,16 +95,16 @@
 		                        //we verify if the image has been uploaded, and print error instead                                                     
 		                       	$copied = copy($_FILES['your_photo']['tmp_name'], $newname);                                                        
 
-		                       
+		                       /*
 		                        if (!$copied)                                                       
 		                        {                                                       
 		                            debug_echo ("Sorry, The Photo Upload was unsuccessfull!"."\n");                                                          
 		                            break;                                                         
 		                        }
 		                        else{
-
+								*/
 		                        	debug_echo ("file uploaded successfull!"."\n"); 
-/*
+
 									//Insert into database.Just use this particular variable "$image_name" when you are inserting into database
 	    						    $insert_image_sql="INSERT INTO photograph (large_url) VALUES ( ? )"; 
 
@@ -129,7 +129,7 @@
 										debug_echo ("photo has been successfully uploaded... "."\n");                                                          
 		                            	break;  
 									}
-*/
+
 		                        }
 
 		                    }                                               
