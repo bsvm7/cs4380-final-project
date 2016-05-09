828158917e5e1d8cf1c62f325dd80ece25a4b5c4
Tony Forsythe <forsythetony@gmail.com>
2016-05-08 23:20:40 -0500

    Fix

--- db/utility/test_query.sql
+++ db/utility/test_query.sql
@@ -1,4 +1,24 @@
 use photoarchiving;
 
 
-select U.ps_id from user U , user_repo UR where r_id = 1;
+INSERT INTO photo_story (p_id , s_id ) values ( 31 , 18 );
+INSERT INTO photo_story (p_id , s_id ) values ( 31 , 18 );
+INSERT INTO photo_story (p_id , s_id ) values ( 19 , 34 );
+INSERT INTO photo_story (p_id , s_id ) values ( 20 , 41 );
+INSERT INTO photo_story (p_id , s_id ) values ( 21 , 22 );
+INSERT INTO photo_story (p_id , s_id ) values ( 22 , 23 );
+INSERT INTO photo_story (p_id , s_id ) values ( 24 , 25 );
+INSERT INTO photo_story (p_id , s_id ) values ( 26 , 27 );
+INSERT INTO photo_story (p_id , s_id ) values ( 28 , 29 );
+INSERT INTO photo_story (p_id , s_id ) values ( 29 , 30 );
+INSERT INTO photo_story (p_id , s_id ) values ( 30 , 31 );
+INSERT INTO photo_story (p_id , s_id ) values ( 31 , 32 );
+INSERT INTO photo_story (p_id , s_id ) values ( 32 , 33 );
+INSERT INTO photo_story (p_id , s_id ) values ( 33 , 34 );
+INSERT INTO photo_story (p_id , s_id ) values ( 34 , 35 );
+INSERT INTO photo_story (p_id , s_id ) values ( 35 , 36 );
+INSERT INTO photo_story (p_id , s_id ) values ( 36 , 37 );
+INSERT INTO photo_story (p_id , s_id ) values ( 37 , 38 );
+INSERT INTO photo_story (p_id , s_id ) values ( 38 , 39 );
+INSERT INTO photo_story (p_id , s_id ) values ( 39 , 40 );
+INSERT INTO photo_story (p_id , s_id ) values ( 40 , 41 );
