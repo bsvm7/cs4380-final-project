9c3ee2c220914cb8a12a1354be16f4e80c37c6ea
wldzc <weijian.li.china@gmail.com>
2016-05-08 22:11:35 -0500

    Test

--- www/photograph.php
+++ www/photograph.php
@@ -102,7 +102,7 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-8">
+			<div class="col-sm-8" style="background-color: blue">
 			<h1>Photo</h1>
 			<table class="table">
 			<thead>
@@ -116,7 +116,7 @@
 			
 			
 			</div>
-			<div class="col-sm-4">
+			<div class="col-sm-4" style="background-color: blue">
 			<h1>Story</h1>
 			<table class="table">
 			<thead>
