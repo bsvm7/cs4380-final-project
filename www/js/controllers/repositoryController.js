819171f89f9828c9073ba0e4e4435a5d617ec3e2
Tony Forsythe <forsythetony@gmail.com>
2016-05-08 20:14:57 -0500

    Fix

--- www/js/controllers/repositoryController.js
+++ www/js/controllers/repositoryController.js
@@ -4,128 +4,12 @@ var app = angular.module('photoarchiving_app', [])
 	//	CONSTANTS
 	var base_url = "http://40.86.85.30/cs4380/";
 	
-	$scope.credentials = {
-		'username' : '',
-		'password' : ''
-		
+	$scope.repo_info = {
+		"title"		: "untitled",
+		"description" : "description",
+		""
 	};
 	
-	checkIfLoggedIn();
-
-	function checkIfLoggedIn() {
-		
-		if (typeof(Storage) !== "undefined") {
-			
-			if (sessionStorage.auth_token) {
-				$window.location.href = base_url + "home.php";
-			}
-		}
-		
-	}
-	
-	
-
-	$scope.authenticate = function() {
-		
-		var auth_url = base_url + "api/login_function.php";
-		
-		var post_body = {
-			"auth_type"		: "initial",
-			"username" 		: $scope.credentials.username,
-			"password"		: $scope.credentials.password,
-			"refresh_token"	: ""
-		};
-		
-		
-		
-		$http.post( auth_url , post_body ).then( function successCallback( response ) {
-						
-			if (response.status == 200 ) {
-				
-				var res_data = response.data;
-				console.log( res_data );
-				
-				//	Gather the response information
-				var res_ps_id 			= res_data.ps_id;
-				var res_username 		= res_data.username;
-				var res_access_token 	= res_data.access_token;
-				var res_expires_in 		= res_data.expires_in;
-				var res_refresh_token 	= res_data.refresh_token;
-				var res_user_level		= res_data.user_level;
-				
-				//	Store these values in session storage
-				store_value_for_key_in_session_storage( "ps_id" , res_ps_id );
-				store_value_for_key_in_session_storage( "username" , res_username );
-				store_value_for_key_in_session_storage( "access_token" , res_access_token );
-				store_value_for_key_in_session_storage( "expires_in" , res_expires_in );
-				store_value_for_key_in_session_storage( "refresh_token" , res_refresh_token );
-				store_value_for_key_in_session_storage( "user_level" , res_user_level );
-				//	Create the redirect URL
-				var redirect_url = base_url + "home.php";
-				
-				$window.location.href = redirect_url;	
-			}
-			else {
-				
-				//	THIS SHOULD BE FIXED
-				
-				var res_data = response.data;
-				console.log("This is the else statement ");
-				console.log( res_data );
-				
-				//	Gather the response information
-				var res_ps_id 			= res_data.ps_id;
-				var res_username 		= res_data.username;
-				var res_access_token 	= res_data.access_token;
-				var res_expires_in 		= res_data.expires_in;
-				var res_refresh_token 	= res_data.refresh_token;
-				var res_user_level		= res_data.user_level;
-				
-				//	Store these values in session storage
-				store_value_for_key_in_session_storage( "ps_id" , res_ps_id );
-				store_value_for_key_in_session_storage( "username" , res_username );
-				store_value_for_key_in_session_storage( "access_token" , res_access_token );
-				store_value_for_key_in_session_storage( "expires_in" , res_expires_in );
-				store_value_for_key_in_session_storage( "refresh_token" , res_refresh_token );
-				store_value_for_key_in_session_storage( "user_level" , res_user_level );
-				//	Create the redirect URL
-				var redirect_url = base_url + "home.php";
-				
-				$window.location.href = redirect_url;				
-			}
-			
-			
-			
-		}, function errorCallback( response ) {
-			
-			//	THIS SHOULD BE FIXED
-				
-				var res_data = response.data;
-				console.log("This is the error callback");
-				
-				//	Gather the response information
-				var res_ps_id 			= res_data.ps_id;
-				var res_username 		= res_data.username;
-				var res_access_token 	= res_data.access_token;
-				var res_expires_in 		= res_data.expires_in;
-				var res_refresh_token 	= res_data.refresh_token;
-				var res_user_level		= res_data.user_level;
-				
-				//	Store these values in session storage
-				store_value_for_key_in_session_storage( "ps_id" , res_ps_id );
-				store_value_for_key_in_session_storage( "username" , res_username );
-				store_value_for_key_in_session_storage( "access_token" , res_access_token );
-				store_value_for_key_in_session_storage( "expires_in" , res_expires_in );
-				store_value_for_key_in_session_storage( "refresh_token" , res_refresh_token );
-				store_value_for_key_in_session_storage( "user_level" , res_user_level );
-				//	Create the redirect URL
-				var redirect_url = base_url + "home.php";
-				
-				$window.location.href = redirect_url;	
-			
-		});
-		
-	}
 	
 	function get_base_url() {
 		
@@ -150,6 +34,33 @@ var app = angular.module('photoarchiving_app', [])
 		}
 		
 		
+	}
+	
+	function get_session_information() {
+		
+		var ret_dict;
+		
+		if (typeof(Storage) !== "undefined") {
+			
+			var auth_token 		= sessionStorage.access_token;
+			var expires_in 		= sessionStorage.expires_in;
+			var ps_id			= sessionStorage.ps_id;
+			var refresh_token 	= sessionStorage.refresh_token;
+			var user_level 		= sessionStorage.user_level;
+			var username 		= sessionStorage.username;
+			
+			
+			ret_dict = {
+				"access_token" 		: auth_token,
+				"expires_in" 		: expires_in,
+				"ps_id"				: ps_id,
+				"user_level"		: user_level,
+				"username"			: username
+			};
+			
+		}
+		
+		return ret_dict;
 	}
 	
   });
