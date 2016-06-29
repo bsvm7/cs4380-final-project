18b9abea38f8cbabeb3894131aded0ae651d3c29
Tony Forsythe <forsythetony@gmail.com>
2016-06-29 17:13:47 -0500

    hi

--- db/utility/create_salt_and_hash.php
+++ db/utility/create_salt_and_hash.php
@@ -1,22 +1,4 @@
 <?php
-	//	Include reference to sensitive databse information
-	include("../../../db_security/security.php");
-	
-	$db_user 		= constant("DB_USER");
-	$db_host 		= constant("DB_HOST");
-	$db_pass 		= constant("DB_PASS");
-	$db_database 	= constant("DB_DATABASE");
-	
-	//	First connect to the database using values from the included file
-	$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_database);
-	
-	if ($db_conn->error_code) {
-		
-		set_error_response( 400 , "I couldn't connect to the database -> " . $db_conn->connect_error);
-		die("The connection to the database failed: " . $db_conn->connect_error);
-	}
-	
-	
 	//	First make sure there is at least one argument passed to the script
 	
 	if($argc != 2)
