be23c6d0c46a7f7cef1ac5217a95819acfaad5d7
Tony Forsythe <forsythetony@gmail.com>
2016-06-29 17:23:55 -0500

    Fix

--- www/js/controllers/loginController.js
+++ www/js/controllers/loginController.js
@@ -67,61 +67,22 @@ var app = angular.module('photoarchiving_app', [])
 			}
 			else {
 				
-				//	THIS SHOULD BE FIXED
+				var	error_message = "I couldn't log you in with the given username and password...";
 				
-				var res_data = response.data;
-				console.log("This is the else statement ");
-				console.log( res_data );
+				console.log( error_message );
 				
-				//	Gather the response information
-				var res_ps_id 			= res_data.ps_id;
-				var res_username 		= res_data.username;
-				var res_access_token 	= res_data.access_token;
-				var res_expires_in 		= res_data.expires_in;
-				var res_refresh_token 	= res_data.refresh_token;
-				var res_user_level		= res_data.user_level;
 				
-				//	Store these values in session storage
-				store_value_for_key_in_session_storage( "ps_id" , res_ps_id );
-				store_value_for_key_in_session_storage( "username" , res_username );
-				store_value_for_key_in_session_storage( "access_token" , res_access_token );
-				store_value_for_key_in_session_storage( "expires_in" , res_expires_in );
-				store_value_for_key_in_session_storage( "refresh_token" , res_refresh_token );
-				store_value_for_key_in_session_storage( "user_level" , res_user_level );
-				//	Create the redirect URL
-				var redirect_url = base_url + "home.php";
-				
-				$window.location.href = redirect_url;				
 			}
 			
 			
 			
 		}, function errorCallback( response ) {
 			
-			//	THIS SHOULD BE FIXED
+			var	error_message = "I couldn't log you in with the given username and password...";
 				
-				var res_data = response.data;
-				console.log("This is the error callback");
+			console.log( error_message );
 				
-				//	Gather the response information
-				var res_ps_id 			= res_data.ps_id;
-				var res_username 		= res_data.username;
-				var res_access_token 	= res_data.access_token;
-				var res_expires_in 		= res_data.expires_in;
-				var res_refresh_token 	= res_data.refresh_token;
-				var res_user_level		= res_data.user_level;
-				
-				//	Store these values in session storage
-				store_value_for_key_in_session_storage( "ps_id" , res_ps_id );
-				store_value_for_key_in_session_storage( "username" , res_username );
-				store_value_for_key_in_session_storage( "access_token" , res_access_token );
-				store_value_for_key_in_session_storage( "expires_in" , res_expires_in );
-				store_value_for_key_in_session_storage( "refresh_token" , res_refresh_token );
-				store_value_for_key_in_session_storage( "user_level" , res_user_level );
-				//	Create the redirect URL
-				var redirect_url = base_url + "home.php";
-				
-				$window.location.href = redirect_url;	
+
 			
 		});
 		
