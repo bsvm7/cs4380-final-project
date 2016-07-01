76c52205df1be680d8457171bf65ea68173e0ade
Tony Forsythe <forsythetony@gmail.com>
2016-06-30 21:03:17 -0500

    Fix

--- www/api/photo.php
+++ www/api/photo.php
@@ -326,9 +326,10 @@
 							
 							//	Create the sql for this query
 							
-							$select_photos_in_era_sql = "SELECT DISTINCT P.p_id, P.title, P.description, P.large_url, P.thumb_url, P.date_taken, P.date_conf, P.date_uploaded, P.uploaded_by, R.r_id "
-														. "FROM photograph P, repository R, photo_repo PR "
-														. "WHERE P.p_id = PR.p_id AND PR.r_id = ? "
+							$select_photos_in_era_sql = "SELECT DISTINCT P.p_id, P.title, P.description, P.large_url, P.thumb_url, P.date_taken, P.date_conf, P.date_uploaded, P.uploaded_by"
+														. "FROM photograph P INNER JOIN photo_repo PR "
+														. "ON P.p_id = PR.p_id "
+														. "WHERE PR.r_id = ? "
 														. "AND P.date_taken >= (SELECT start_date FROM era WHERE era_id = ?) "
 														. "AND P.date_taken <= (SELECT end_date FROM era WHERE era_id = ?) ";
 														
