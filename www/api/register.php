ae246f7015f545adc726108aec95e66aa7045f1e
Tony Forsythe <forsythetony@gmail.com>
2016-06-30 19:30:01 -0500

    Fix

--- www/api/register.php
+++ www/api/register.php
@@ -52,8 +52,8 @@
 				array_push($test_arr, $registration_info->middle_name()); //
 				array_push($test_arr, $registration_info->maiden_name()); //
 				array_push($test_arr, $registration_info->username()); //
-				// array_push($test_arr, $registration_info->password());
-				array_push($test_arr, $registration_info->email());
+				array_push($test_arr, $registration_info->password());
+				//array_push($test_arr, $registration_info->email());
 				
 				echo var_dump($test_arr);
 /*
